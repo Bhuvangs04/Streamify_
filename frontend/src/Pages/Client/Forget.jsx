950b42d46c705debe677be32bfac4c003dd8b083
Bhuvangs04 <bhuvangs2004@gmail.com>
2025-01-03 22:38:59 +0530

    Update Forget.jsx

--- frontend/src/Pages/Client/Forget.jsx
+++ frontend/src/Pages/Client/Forget.jsx
@@ -26,7 +26,7 @@ export default function ForgetPage() {
     setErrorMessage("");
 
     try {
-      await axios.post("http://localhost:8081/api/user/forgot-password", {
+      await axios.post("https://streamify-694k.onrender.com/api/user/forgot-password", {
         email,
       });
       setIsSuccess(true);
