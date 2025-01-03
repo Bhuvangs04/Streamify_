7f513ab22cfd9a47f75745124cc3cd43c0e62f53
Bhuvangs04 <bhuvangs2004@gmail.com>
2025-01-03 23:04:12 +0530

    Update UserModal.jsx

--- frontend/src/Pages/Admin/UserModal.jsx
+++ frontend/src/Pages/Admin/UserModal.jsx
@@ -37,7 +37,7 @@ const UserDetailsModal = ({ isOpen, onClose, userDetails }) => {
     setIsSending(true);
     try {
       await axios.post(
-        `http://localhost:8081/api/admin/reports/send-email/${userDetails._id}`,
+        `https://streamify-694k.onrender.com/api/admin/reports/send-email/${userDetails._id}`,
         {
           content: emailContent,
         },
