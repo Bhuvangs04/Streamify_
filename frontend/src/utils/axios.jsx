66efb0714ca7c438f19e5f08f39c37fa804202de
Bhuvangs04 <bhuvangs2004@gmail.com>
2025-01-03 22:36:31 +0530

    Update axios.jsx

--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const instance = axios.create({
-  baseURL: "http://localhost:8081/api", // Update with your API base URL
+  baseURL: "https://streamify-694k.onrender.com/api", // Update with your API base URL
   withCredentials: true, // If you need to send credentials like cookies or tokens
 });
 
