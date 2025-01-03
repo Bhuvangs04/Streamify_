e937ba6558149b0e940c6e2aea3996f160830624
Bhuvangs04 <bhuvangs2004@gmail.com>
2025-01-03 22:10:11 +0530

    Update redis.js

--- backend/routes/config/redis.js
+++ backend/routes/config/redis.js
@@ -1,13 +1,16 @@
+// Load environment variables from .env file
+require('dotenv').config();
+
 const Redis = require("ioredis");
 
 const redisClient = new Redis({
-  host: "127.0.0.1", // Redis host
-  port: 6379, // Default Redis port
+  host: process.env.REDIS_HOST, 
+  port: process.env.REDIS_PORT, 
+  password: process.env.REDISCLI_AUTH, 
+  username: process.env.REDIS_USER, 
   maxRetriesPerRequest: 3,
   retryStrategy: (times) => Math.min(times * 50, 2000),
   connectTimeout: 10000,
 });
-
-
 
 module.exports = redisClient;
