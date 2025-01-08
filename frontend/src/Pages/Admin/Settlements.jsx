8763180b90561142e6408f9f4a4fe06b10ab70f9
Bhuvan G Sangappanavar <155778797+bhuvan07@users.noreply.github.com>
2025-01-08 19:00:22 +0530

    Update Settlements.jsx

--- frontend/src/Pages/Admin/Settlements.jsx
+++ frontend/src/Pages/Admin/Settlements.jsx
@@ -15,7 +15,7 @@ const SettlementsPage = () => {
     queryFn: async () => {
       try {
         const response = await axios.get(
-          `https://streamify-694k.onrender.com/api/payment/payment/settlement`,
+          `https://streamify-o1ga.onrender.com/api/payment/payment/settlement`,
           { withCredentials: true }
         );
         return response.data.items || [];
