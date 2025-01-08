33d38f062642185dd9143bb1a6548b0d4a7a3a79
Bhuvan G Sangappanavar <155778797+bhuvan07@users.noreply.github.com>
2025-01-08 19:01:22 +0530

    Update UserProfilePage.jsx

--- frontend/src/Pages/Admin/UserProfilePage.jsx
+++ frontend/src/Pages/Admin/UserProfilePage.jsx
@@ -35,7 +35,7 @@ const UserProfile = () => {
       const currentDate = getCurrentDate();
       await new Promise((resolve) => setTimeout(resolve, 3000));
       const response = await axios.get(
-        `https://streamify-694k.onrender.com/api/admin/send/all/detailed/${currentDate}/fetchAll`
+        `https://streamify-o1ga.onrender.com/api/admin/send/all/detailed/${currentDate}/fetchAll`
       );
       return response.data;
     },
