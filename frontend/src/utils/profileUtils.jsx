5359b9de03fa70504b43fdeeff6c83c7e2bbadf2
Bhuvan G Sangappanavar <155778797+bhuvan07@users.noreply.github.com>
2025-01-08 19:16:04 +0530

    Update profileUtils.jsx

--- frontend/src/utils/profileUtils.jsx
+++ frontend/src/utils/profileUtils.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const checkPaymentStatus = async () => {
   try {
     const response = await axios.get(
-      "https://streamify-694k.onrender.com/api/payment/check-payment",
+      "https://streamify-o1ga.onrender.com/api/payment/check-payment",
       { withCredentials: true }
     );
     return response.data;
