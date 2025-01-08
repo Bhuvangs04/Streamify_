b0d87652a9335f264517bc0a239e479ddec7df1b
Bhuvan G Sangappanavar <155778797+bhuvan07@users.noreply.github.com>
2025-01-08 18:55:04 +0530

    Update AdminAnalysis.jsx

--- frontend/src/Pages/Admin/AdminAnalysis.jsx
+++ frontend/src/Pages/Admin/AdminAnalysis.jsx
@@ -34,7 +34,7 @@ const AdminAnalytics = () => {
     queryKey: ["analytics"],
     queryFn: async () => {
       const response = await axios.post(
-        "https://streamify-694k.onrender.com/api/admin/analytics/details",
+        "https://streamify-o1ga.onrender.com/api/admin/analytics/details",
         {
           withCredentials: true,
         }
