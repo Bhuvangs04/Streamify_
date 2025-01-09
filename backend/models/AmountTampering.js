9a864cd2d0c0cc9ee0139c2c2017c369c0fc37bb
Bhuvan G Sangappanavar <155778797+bhuvan07@users.noreply.github.com>
2025-01-09 20:16:45 +0530

    Create AmountTampering.js

---
+++ backend/models/AmountTampering.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const suspiciousOrderSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  orderId: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  status:{
+    type:String,
+    required:true
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  tamperingType: {
+    type: String,
+    required: true,
+  },
+  isSuspicious: {
+    type: Boolean,
+    default: true,
+  },
+  additionalInfo: {
+    type: String,
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const SuspiciousOrder = mongoose.model(
+  "SuspiciousOrder",
+  suspiciousOrderSchema
+);
+
+module.exports = SuspiciousOrder;
