39ac09265dbb491be65dd0de2d8a9c94bc1c946b
Bhuvan G Sangappanavar <155778797+bhuvan07@users.noreply.github.com>
2025-01-09 20:19:22 +0530

    Update payhistory.js

--- backend/models/payhistory.js
+++ backend/models/payhistory.js
@@ -14,6 +14,13 @@ const historyPaymentSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+   ipAddress: { type: String, required: true },
+    city: { type: String, required: true },
+    country: {type:String,required:true},
+    network: {type:String,required:true},
+    version: {type:String,required:true},
+    latitude: {type:String,required:true},
+    longitude: {type:String,required:true},
   currency: {
     type: String,
     required: true,
