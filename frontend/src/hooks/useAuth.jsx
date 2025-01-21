b6b649da75b37ebbdb0e67fbe507ba494d4d1c3f
Bhuvan G Sangappanavar <155778797+bhuvan07@users.noreply.github.com>
2025-01-21 11:24:23 +0530

    Update useAuth.jsx

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -5,7 +5,7 @@ axios.defaults.withCredentials = true;
 
 const verifyAuth = async () => {
   const response = await axios.get(
-    "https://streamizz.site/api/netflix/admin/verify-auth",
+    "https://streamify-o1ga.onrender.com/api/netflix/admin/verify-auth",
     {
       withCredentials: true,
     }
